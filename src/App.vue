ee8b96fe394c0448effb603a990430ffd50a98f8
K1A2 <jckim0307@gmail.com>
2022-11-20 19:35:47 +0900

    [FEAT] 상단바 구현 완료

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,80 @@ export default { }
 
 <style>
 @import "https://fonts.googleapis.com/icon?family=Material+Icons+Outlined";
+@import "https://fonts.googleapis.com/icon?family=Material+Icons+Round";
+
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 100;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream1.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream1.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 200;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream2.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream2.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 300;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream3.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream3.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 400;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream4.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream4.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 500;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream5.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream5.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 600;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream6.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream6.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 700;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream7.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream7.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 800;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream8.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream8.woff) format('woff');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'SCoreDream';
+  font-weight: 900;
+  font-style: normal;
+  src: url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream9.woff2) format('woff2'),
+  url(https://cdn.jsdelivr.net/gh/webfontworld/SCoreDream/SCoreDream9.woff) format('woff');
+  font-display: swap;
+}
 
 .icon-color-button-gray {
   color: #C7C7C7;
@@ -19,6 +93,7 @@ html {
   background: #F1F1F1;
   margin: 0;
   padding: 0;
+  font-family: 'SCoreDream';
 }
 body {
   margin: 0;
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -8,8 +8,16 @@
     <!-- <a href="#/main" class="">main</a> -->
 <!--  </nav>-->
   <nav class="navbar-top">
+    <div class="navbar-right">
+      <span class="material-icons-round">menu</span>
+      <div class="navbar-title">아파트 통합 시스템</div>
+    </div>
     <div class="navbar-alert">
       <span class="material-icons-outlined icon-color-button-gray">notifications</span>
+    </div>
+    <div class="navbar-user-left">
+      <div class="navbar-user-name">userName</div>
+      <div class="navbar-user-profile"></div>
     </div>
   </nav>
 </template>
@@ -21,7 +29,6 @@ export default {
 </script>
 
 <style scoped>
-
 .navbar-top {
   background: #FFFFFF;
   display: flex;
@@ -30,5 +37,69 @@ export default {
   align-items: center;
   padding: 16px 20px;
   gap: 19px;
+}
+.navbar-title {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #000000;
+}
+
+.navbar-right {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  order: 0;
+  gap: 19px;
+  align-items: center;
+}
+
+.navbar-alert {
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  width: 25px;
+  height: 25px;
+}
+
+.navbar-user-left {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 10px;
+
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+}
+
+.navbar-user-name {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+
+  color: #393939;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+}
+
+.navbar-user-profile {
+  width: 32px;
+  height: 32px;
+
+  background-image: url('../assets/test_profile.png');
+  background-position: center;
+  border-radius: 16px;
+  background-size: 100%;
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 }
 </style>
