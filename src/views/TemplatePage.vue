7f21d80570baf167fb998e7eada04dc30a432d35
K1A2 <jckim0307@gmail.com>
2022-11-22 11:05:18 +0900

    [FEAT] template 구현

---
+++ src/views/TemplatePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <NavigationBar></NavigationBar>
+  <div id="section-main">
+    <SideBar></SideBar>
+    <div id="section-main-content">
+      <!--TODO:여기에 레이아웃 구현-->
+      <h1>temp</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavigationBar from "@/components/NavigationBar";
+import SideBar from "@/components/sidebar/SideBar";
+export default {
+  //TODO: name 변경
+  name: "TempPage",
+  components: {SideBar, NavigationBar},
+}
+</script>
+
+<style>
+#section-main {
+  display: flex;
+  padding: 0;
+  /*overflow-y: scroll;*/
+}
+
+#section-main-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0;
+  gap: 38px;
+
+  flex: 1;
+  order: 1;
+}
+</style>
