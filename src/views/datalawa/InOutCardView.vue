a5862842980e3a3711058038d704fbc0d5cd49b7
EJLimm <amy020413@hanyang.ac.kr>
2022-11-23 10:34:08 +0900

    init

---
+++ src/views/datalawa/InOutCardView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="content-box">
+        <h3>{{carnum}}</h3>
+        <p>{{intime}}</p>
+        <br>
+        <nav>
+        <router-link to="/datalawa/mycar" style ="style1">본인 차량 조회</router-link>|
+        <router-link to="/datalawa/inout" style ="style1">입출차 기록</router-link>
+        </nav>
+  <router-view/>
+    </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      carnum: '12가 1234',
+      intime: '01:01:00',
+      style1: {
+        fontsize: '20px'
+      }
+    }
+  },
+  setup() {},
+  created() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+<style scoped>
+.content-box{
+    position: absolute;
+    width: 236px;
+    height: 182px;
+    left: 328px;
+    top: 131px;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.08);
+    border-radius: 18px;
+}
+</style>
---
+++ src/views/datalawa/MyCarView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
