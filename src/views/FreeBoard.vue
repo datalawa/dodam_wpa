94624454e9de919fba71b5407357732dedb441f0
K1A2 <jckim0307@gmail.com>
2022-11-26 18:59:06 +0900

    [FEAT] 자유게시판 리스트 구현 완료

--- src/views/FreeBoard.vue
+++ src/views/FreeBoard.vue
@@ -14,42 +14,9 @@
           </div>
         </div>
         <div class="card background-shadow section-board-content-root">
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
-          <BoardItemType1></BoardItemType1>
-          <div class="section-board-content-divider"></div>
+          <BoardItemType1 v-for="item in getTestArticleList" v-bind:key="item"
+                          :title="item.title" :content="item.content" :author="item.author"
+                          :write-time="item.writeTime" :like-count="item.like" :comment-count="item.comment"></BoardItemType1>
         </div>
       </div>
     </div>
@@ -63,6 +30,27 @@ import BoardItemType1 from "@/components/list/BoardItemType1";
 export default {
   name: "FreeBoard",
   components: {BoardItemType1, SideBar, NavigationBar},
+  computed: {
+    getTestArticleList() {
+      let articleDatas = []
+      let writeTime = new Date();
+
+      for (let i = 0;i < 50;i++) {
+        const author = Math.random().toString(36).substring(2, 12)
+        writeTime = new Date(writeTime - ((Math.random(24 * 60) + 90) * 60000))
+
+        articleDatas.push({
+          title: '제목' + i,
+          content: '내용' + i,
+          author: author,
+          writeTime: writeTime,
+          like: Math.floor(Math.random() * 100),
+          comment: Math.floor(Math.random() * 100),
+        })
+      }
+      return articleDatas
+    }
+  }
 }
 </script>
 
