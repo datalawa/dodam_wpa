1b262f2c5840a9f14b9f45233210cc72b155c839
K1A2 <jckim0307@gmail.com>
2022-12-02 15:38:24 +0900

    [FEAT] dashboard board 구현 완료

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -10,6 +10,25 @@
           <DashboardCards icon="local_parking" middle-text="11.12 13:24:52" bottom-text="최근 입차 시각" end-text="32가 1234"></DashboardCards>
           <DashboardCards></DashboardCards>
         </div>
+        <div class="dashboard-section-graph card background-shadow">
+          <div class="dashboard-text-title">납부금액 추이</div>
+        </div>
+        <div class="dashboard-section-notice card background-shadow">
+          <div class="dashboard-text-title">최근 공지사항</div>
+          <div class="section-board-content-root dashboard-section-article-list">
+            <BoardItemType2 v-for="item in getTestNoticeArticleList" v-bind:key="item"
+                            :title="item.title" :author="item.author"
+                            :write-time="item.writeTime"></BoardItemType2>
+          </div>
+        </div>
+        <div class="dashboard-section-free card background-shadow">
+          <div class="dashboard-text-title">자유게시판 인기글</div>
+          <div class="section-board-content-root dashboard-section-article-list">
+            <BoardItemType1 v-for="item in getTestFreeArticleList" v-bind:key="item"
+                            :title="item.title" :content="item.content" :author="item.author"
+                            :write-time="item.writeTime" :like-count="item.like" :comment-count="item.comment"></BoardItemType1>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,9 +38,48 @@
 import NavigationBar from "@/components/NavigationBar";
 import SideBar from "@/components/sidebar/SideBar";
 import DashboardCards from "@/components/DashboardCards";
+import BoardItemType1 from "@/components/list/BoardItemType1";
+import BoardItemType2 from "@/components/list/BoardItemType2";
 export default {
   name: "DashBoard",
-  components: {DashboardCards, SideBar, NavigationBar},
+  components: {BoardItemType2, BoardItemType1, DashboardCards, SideBar, NavigationBar},
+  computed: {
+    getTestFreeArticleList() {
+      let articleDatas = []
+      let writeTime = new Date();
+
+      for (let i = 0;i < 5;i++) {
+        const author = Math.random().toString(36).substring(2, 12)
+        writeTime = new Date(writeTime - ((Math.random(24 * 60) + 90) * 60000))
+
+        articleDatas.push({
+          title: '제목' + i,
+          content: '내용' + i,
+          author: author,
+          writeTime: writeTime,
+          like: Math.floor(Math.random() * 100),
+          comment: Math.floor(Math.random() * 100),
+        })
+      }
+      return articleDatas
+    },
+    getTestNoticeArticleList() {
+      let articleDatas = []
+      let writeTime = new Date();
+
+      for (let i = 0;i < 5;i++) {
+        const author = Math.random().toString(36).substring(2, 12)
+        writeTime = new Date(writeTime - ((Math.random(24 * 60) + 90) * 60000))
+
+        articleDatas.push({
+          title: '제목' + i,
+          author: author,
+          writeTime: writeTime.toISOString().substring(0, 10),
+        })
+      }
+      return articleDatas
+    }
+  },
 }
 </script>
 
