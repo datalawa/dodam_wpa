bc1e2a36080ae0cf92b357041c0f414cef3218dc
K1A2 <jckim0307@gmail.com>
2022-12-06 15:45:02 +0900

    [FEAT] car inout page 생성

--- src/views/CarInOutPage.vue
+++ src/views/CarInOutPage.vue
@@ -21,7 +21,51 @@
             <InOutCard></InOutCard>
             <InOutCard></InOutCard>
           </div>
-          <InOutListView></InOutListView>
+          <div class="card background-shadow inout-root">
+            <div class="section-inout-title">{{ selectedCarNum }} 입출차 기록</div>
+            <v-table class="content-box">
+              <thead>
+              <tr>
+                <th>
+                  번호
+                </th>
+                <th>
+                  날짜
+                </th>
+                <th>
+                  동
+                </th>
+                <th>
+                  호수
+                </th>
+                <th>
+                  차종
+                </th>
+                <th>
+                  구분
+                </th>
+                <th>
+                  입차시간
+                </th>
+                <th>
+                  출차시간
+                </th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr :key="i.idx" v-for="i in (0,20)">
+                <td>{{ i }}</td>
+                <td><InOutData :date="date"/>date</td>
+                <td><InOutData :dong="dong"/>dong</td>
+                <td><InOutData :ho="ho"/>ho</td>
+                <td><InOutData :kind="kind"/>kind</td>
+                <td><InOutData :visit="visit"/>visit</td>
+                <td><InOutData :intime="intime"/>intime</td>
+                <td><InOutData :outtime="outtime"/>outtime</td>
+              </tr>
+              </tbody>
+            </v-table>
+          </div>
         </div>
       </div>
     </div>
@@ -36,6 +80,11 @@ import InOutListView from "@/views/datalawa/InOutListView.vue";
 export default {
   name: "CarInOutPage",
   components: {InOutListView, InOutCard, SideBar, NavigationBar},
+  data: () => {
+    return {
+      selectedCarNum: '12가 3456'
+    }
+  }
 }
 </script>
 
@@ -47,5 +96,13 @@ export default {
 <style scoped>
 .section-fee-contents {
   flex-direction: column !important;
+}
+
+.inout-root {
+  padding: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 </style>
