0404ab9cf6f5e2cf0dbbdfdec154c8c85cac70c5
Ahyeon Lee <laah0602@gmail.com>
2022-12-12 09:18:45 +0900

    [FEAT] 납부기록 화면 구현 완료

--- src/views/datalawa/PayListView.vue
+++ src/views/datalawa/PayListView.vue
@@ -1,35 +1,37 @@
 <template>
     <NavigationBar></NavigationBar>
-    <div id="section-main">
-      <SideBar :is-hidden="true"></SideBar>
-      <div id="section-main-transparent"></div>
+    <div id="section-main" class="open">
+      <SideBar :is-hidden="false"></SideBar>
+      <div id="section-main-transparent" class="open"></div>
       <div id="section-main-content">
-        <div>
-        <h3>납부기록</h3>
-        총 1건의 납부한 결과가 있습니다.
-          <v-table class="content-box">
-          <thead>
-              <tr>
-              <th>
-                  납부상태
-              </th>
-              <th>
-                  날짜
-              </th>
-              <th>
-                  납부액
-              </th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr :key="i.idx" v-for="i in (0,20)">
-                    <td><PayData :payox="payox"/>payox {{payox}}</td>
-                    <td><PayData :date="date"/>date {{date}}</td>
-                    <td><PayData :cost="cost"/>cost {{cost}}</td>
-              </tr>
-          </tbody>
-          </v-table>
+        <div class="section-board-root">
+          <div class="section-board-top">
+            <h3>납부기록</h3>
+            총 1건의 납부한 결과가 있습니다.
+            <v-table class="content-box">
+            <thead>
+                <tr>
+                <th>
+                    납부상태
+                </th>
+                <th>
+                    날짜
+                </th>
+                <th>
+                    납부액
+                </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="i.idx" v-for="i in (0,20)">
+                        <td><PayData :payox="payox"/>payox {{payox}}</td>
+                        <td><PayData :date="date"/>date {{date}}</td>
+                        <td><PayData :cost="cost"/>cost {{cost}}</td>
+                </tr>
+            </tbody>
+            </v-table>
         </div>
+      </div>
     </div>
     </div>
   </template>
